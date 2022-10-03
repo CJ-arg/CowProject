70d6daf96cccec220c3de14836a3a4a2ec73d35c
unknown <janoncarlos@gmail.com>
2022-10-02 22:34:40 -0300

    githublink

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,65 +1,11 @@
-/* eslint-disable linebreak-style */
-import { Link, Zoom } from "@material-ui/core";
-// @ts-ignore
+import { Link } from "@material-ui/core";
 import React from "react";
-import CustomTooltip from "./CustomTooltip";
 
 function Info() {
-  const [open, setOpen] = React.useState(false);
-
-  const handleTooltipClose = () => {
-    setOpen(false);
-  };
-
-  const handleTooltipOpen = () => {
-    setOpen(true);
-  };
   return (
-    <CustomTooltip
-      onClose={handleTooltipClose}
-      open={open}
-      disableFocusListener
-      disableHoverListener
-      disableTouchListener
-      title={
-        <ul>
-          <li>
-            <Link href="https://www.linkedin.com/in/carlos-janon-developer/" target="_blank">
-              Linkedin
-            </Link>
-          </li>
-          <br />
-          <li>
-            <Link href="https://github.com/CJ-arg" target="_blank">
-              Github
-            </Link>
-          </li>
-          <br />
-          <li>
-            <Link href="mailto:cowproyectapp@gmail.com">Mail</Link>
-          </li>
-        </ul>
-      }
-      TransitionComponent={Zoom}
-      interactive
-      TransitionProps={{ timeout: 600 }}
-      placement="top-end"
-      PopperProps={{
-        disablePortal: true,
-        modifiers: {
-          offset: {
-            enabled: true,
-            offset: "-100px, 3px",
-          },
-        },
-      }}
-    >
-      <div>
-        <h4 className="latamMode" onClick={open ? handleTooltipClose : handleTooltipOpen}>
-          Info
-        </h4>
-      </div>
-    </CustomTooltip>
+    <Link href="https://github.com/CJ-arg" color="secondary" underline="none" target="_blank" className="link">
+      Github
+    </Link>
   );
 }
 
